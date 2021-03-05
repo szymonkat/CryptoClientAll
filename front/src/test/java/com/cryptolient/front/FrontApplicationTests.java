7292a4dcff07271cfcf6d5e99cab221301cc5b38
szymonkat <szymonkat@gmail.com>
2021-03-05 17:56:08 +0100

    Projects_Interconnection

---
+++ front/src/test/java/com/cryptolient/front/FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptolient.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
