ef9a3c55c3896d84cd5508e11e5dff7d07d71638
szymonkat <szymonkat@gmail.com>
2021-03-10 20:39:40 +0100

    update

--- common/src/main/java/com/cryptoclient/dto/ItemToBuyDto.java
+++ common/src/main/java/com/cryptoclient/dto/ItemToBuyDto.java
@@ -3,10 +3,12 @@ package com.cryptoclient.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class ItemToBuyDto {
 
     private Long id;
@@ -16,5 +18,13 @@ public class ItemToBuyDto {
     public ItemToBuyDto(Long exchangePortalId, Double quantityToBuy) {
         this.exchangePortalId = exchangePortalId;
         this.quantityToBuy = quantityToBuy;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemToBuyDto: " +
+                "id=" + id +
+                ", exchangePortalId=" + exchangePortalId +
+                ", quantityToBuy=" + quantityToBuy;
     }
 }
